37fc50fd3ce7a9439cf5df76241f5483c3ffba18
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-01 02:19:42 +0600

    done Upcoming Book

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -61,6 +61,19 @@ const Home = () => {
         </div>
       </div>
       <Products></Products>
+      <div className="p-20 mt-10 flex w-3/4 mx-auto">
+        <div>
+          <img className="h-3/4 my-auto w-full" src="https://diwanegypt.com/wp-content/uploads/2020/08/9780007155668.jpg" alt="" />
+        </div>
+        <div className="w-4/6 text-left p-10 mx-auto my-20">
+          <h1>Upcoming Book</h1>
+          <h3>The Alchemist</h3>
+          <p>The Alchemist is a classic novel in which a boy named Santiago embarks on a journey seeking treasure in the Egyptian pyramids after having a recurring dream about it and on the way meets mentors, falls in love, and most importantly, learns the true importance of who he is and how to improve himself ...</p>
+          <button className="px-4 py-2 mt-5 rounded-lg hover:bg-blue-800 duration-300 bg-blue-500 text-white ">
+            UpComming
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
