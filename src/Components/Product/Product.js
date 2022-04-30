e69b95fdce658059369284d1e4bf7e6821063b86
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-01 01:57:47 +0600

    done blog section

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,14 +1,8 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./Product.css";
 
 const Product = ({ product }) => {
   const { supplier, name, image, price } = product;
-  const navigate = useNavigate();
-
-  const navigateToProductDetail = (id) => {
-    navigate(`/Product/${id}`);
-  };
   return (
     <div className="border-gray-300 border-2 rounded-lg p-8">
       <img className="w-100" src={image} alt="" />
@@ -16,7 +10,7 @@ const Product = ({ product }) => {
       <p>Price:$ {price}</p>
       <p>supplier: {supplier}</p>
       <button className="px-4 py-2 rounded-lg hover:bg-blue-800 duration-300 bg-blue-500 text-white">
-        Read More
+        Update Now
       </button>
     </div>
   );
