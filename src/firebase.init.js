1210629cad2e85466c6ba9a49cbe716c846a3de2
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-02 19:32:18 +0600

    done firebase init

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
-import {getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0BA_nribLo0MhguMj0yqDsPXKAR_OQ2s",
-  authDomain: "book-store-e6ec1.firebaseapp.com",
-  projectId: "book-store-e6ec1",
-  storageBucket: "book-store-e6ec1.appspot.com",
-  messagingSenderId: "34238047363",
-  appId: "1:34238047363:web:051a288d9012734831a7b6"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 export default auth;
