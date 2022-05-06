e581551a2135743c956f72d96ad35d97bd089bc5
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-07 01:34:09 +0600

    done alhamdulillah

--- src/Components/MyItem/MyItem.js
+++ src/Components/MyItem/MyItem.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const MyItem = () => {
-    return (
-        <div>
-            <h1>MyItem</h1>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>MyItem</h1>
+    </div>
+  );
 };
 
 export default MyItem;
